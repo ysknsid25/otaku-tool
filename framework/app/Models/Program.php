6d82cf219bf357b7b041e453f0687db129089b39
ysknsid25 <kengo071225@gmail.com>
2023-04-02 17:08:40 +0900

    #10 A&G番組情報を保持するseeder,migrationの準備
    モデルの作成
    
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ framework/app/Models/Program.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+    protected $guarded = ['created_at', 'updated_at'];
+}
