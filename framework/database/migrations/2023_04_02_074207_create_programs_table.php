67fc77cbb649c38460d719576b2692cb6e9ee7b5
ysknsid25 <kengo071225@gmail.com>
2023-04-02 17:02:27 +0900

    #10 A&G番組情報を保持するseeder,migrationの準備
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ framework/database/migrations/2023_04_02_074207_create_programs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->string("programnm", 256)->default("");
+            $table->integer('begintime')->nullable();
+            $table->integer('endtime')->nullable();
+            $table->integer('weekday')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programs');
+    }
+};
