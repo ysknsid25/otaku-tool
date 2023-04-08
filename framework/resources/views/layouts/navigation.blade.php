11c94697c7e5284106032cfa0a52106fd7751fa7
ysknsid25 <kengo071225@gmail.com>
2023-04-08 20:24:01 +0900

    fix: モバイルの場合の表示を追加
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- framework/resources/views/layouts/navigation.blade.php
+++ framework/resources/views/layouts/navigation.blade.php
@@ -83,6 +83,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('programs.index')" :active="request()->routeIs('programs.*')">
+                {{ __('A&G Programs') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
