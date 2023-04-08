a6cefe3aad56d08147fbcf749ad2d5ce923d4bf1
ysknsid25 <kengo071225@gmail.com>
2023-04-08 23:19:19 +0900

    #35 TOPページをこのサービスの宣伝ページにする
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ framework/resources/views/components/common-meta.blade.php
@@ -0,0 +1,9 @@
+<title>おたくつーる</title>
+<link rel="icon" href="{{ asset('images/favicon.ico') }}" />
+<meta name="description" content="オタクライフをより豊かにするツール" />
+<meta property="og:description" content="オタクライフをより豊かにするツール" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="og:title" content="おたくつーる" />
+<meta property="og:site_name" content="おたくつーる" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content={{ asset('images/otaku-tool-card.jpeg') }} />
